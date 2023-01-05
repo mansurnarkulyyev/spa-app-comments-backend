91eb7bf0289cba888c60396c2b69700abe677cdf
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2023-01-06 00:13:33 +0200

    fix

--- controllers/comments/getAll.js
+++ controllers/comments/getAll.js
@@ -1,16 +1,23 @@
 const {Post} = require("../../models/post");
 
 
-const getAll = async(req, res) => {//вернем не все книги а тот который добавил пользователь 
-   const {_id: owner} = req.user;
-   const{page = 1, limit = 10} = req.query;
-   const skip = (page - 1) * limit;//пагинация
-   const result = await Post.find({owner}, " -updatedAt",{skip,limit}) //если хотим отдавать какой то поля в массиве то передаем их в скобке на пример "title author name"б а ессли из не передавать то поставим передними минус как в примере "-title -author -name"
-                            .populate("owner", "-password -createdAt -updatedAt")//все данные пользователья включае эмаил
+const getAll=async(_,res) =>{
+    const result = await Post.find();
     res.json(result);
 }
 
 module.exports = getAll;
+
+// const getAll = async(req, res) => {//вернем не все книги а тот который добавил пользователь 
+//    const {_id: owner} = req.user;
+//    const{page = 1, limit = 10} = req.query;
+//    const skip = (page - 1) * limit;//пагинация
+//    const result = await Post.find({owner}, " -updatedAt",{skip,limit}) //если хотим отдавать какой то поля в массиве то передаем их в скобке на пример "title author name"б а ессли из не передавать то поставим передними минус как в примере "-title -author -name"
+//                             .populate("owner", "-password -createdAt -updatedAt")//все данные пользователья включае эмаил
+//     res.json(result);
+// }
+
+// module.exports = getAll;
 
 
 
