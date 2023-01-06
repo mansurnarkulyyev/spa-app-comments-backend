3c1a61611cb814d6c916d4e9e396b1d5a45acddc
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2023-01-06 20:03:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 mongoose.set('strictQuery', true);
-const { DB_HOST, PORT=3007 } = process.env;
+// const { DB_HOST, PORT=3007 } = process.env;
 
 mongoose
-  .connect(DB_HOST)
+  .connect("mongodb+srv://Mansur:hH6mTQM8pH7XCf0k@cluster0.0brwolc.mongodb.net/comments_creator?retryWrites=true&w=majority")
   .then(() => app.listen(PORT, () => console.log("Started ok")))
   .catch((err) => {
     console.log(err.message);
